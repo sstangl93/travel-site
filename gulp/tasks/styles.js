58dd0cd2863e0fcf9e475b6dbfb567922488f1f8
Steve <stevestangl@gmail.com>
2019-01-13 18:04:01 -0500

    Many changes... Lol!

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,10 +3,15 @@ var gulp = require('gulp'),
 	autoprefixer = require('autoprefixer'),
 	cssVars = require('postcss-simple-vars'),
 	cssNested = require('postcss-nested'),
-	cssImport = require('postcss-import');
+	cssImport = require('postcss-import'),
+	cssMixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssNested, cssVars, autoprefixer]))
+		.pipe(postcss([cssImport, cssMixins, cssNested, cssVars, autoprefixer]))
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./app/temp/styles'));
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,5 @@
-var gulp = require('gulp'),
-	watch = require('gulp-watch'),
-	postcss = require('gulp-postcss'),
-	autoprefixer = require('autoprefixer'),
-	cssVars = require('postcss-simple-vars'),
-	cssNested = require('postcss-nested'),
-	cssImport = require('postcss-import'),
-	browserSync = require('browser-sync').create();
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function() {
-	console.log("Hooray! You have created a Gulp task!");
-});
 
-gulp.task('html', function() {
-	console.log("Imagine something useful here...");
-});
 
-gulp.task('styles', function() {
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssNested, cssVars, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
-
-gulp.task('watch', function() {
-
-	browserSync.init({
-		notify: false,
-		server: {
-			baseDir: "app"
-		}
-	});
-	
-	watch('./app/index.html', function() {
-		browserSync.reload();
-	});
-
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('cssInject');
-	});
-
-});
-
-gulp.task('cssInject', ['styles'], function() {
-	return gulp.src('./app/temp/styles/styles.css')
-	.pipe(browserSync.stream());
-});
